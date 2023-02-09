a9b74a67cbf169cbab0a58fceb4ab90e1115d170
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2023-02-09 16:11:18 -0300

    Beginning Front-End track

---
+++ 05-Front-End-Track/01-Front-End-UI/01_1-import-export/moduleA.js
@@ -0,0 +1,9 @@
+const variable = 'Value';
+const variable2 = 'Other Value';
+
+export const variable3 = 'One more Value';
+
+export default {
+  variable: variable,
+  variable2: variable2,
+};
---
+++ 05-Front-End-Track/01-Front-End-UI/01_1-import-export/moduleB.js
@@ -0,0 +1,4 @@
+import ModuleA, {variable3} from './moduleA.js';
+
+console.log(ModuleA);
+console.log(variable3)
